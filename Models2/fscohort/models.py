ba028eb989576e46c66fe6e11d22ed7dd5503c23
m_emin <mehmeteminbul@gmail.com>
2022-05-16 22:18:53 +0300

    field types

--- Models2/fscohort/models.py
+++ Models2/fscohort/models.py
@@ -1,3 +1,22 @@
+from atexit import register
+from distutils.command import upload
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    about = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to='media/')
+    register_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name}-{self.last_name}"
+
+    class Meta:
+        ordering = ["number"]
+        verbose_name_plural = "Öğrenciler"
