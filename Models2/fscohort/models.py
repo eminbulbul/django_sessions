a02eace6b358d38f4a114b7bff34313ec72cb435
m_emin <mehmeteminbul@gmail.com>
2022-05-16 22:24:15 +0300

    field types ended

--- Models2/fscohort/models.py
+++ Models2/fscohort/models.py
@@ -6,6 +6,14 @@ from django.db import models
 
 
 class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES = [
+        ("FR", 'Freshman'),
+        ("SP", 'Sophomore'),
+        ("JR", 'Junior'),
+        ("SR", 'Senior'),
+        ("GRD", 'Graduate'),
+    ]
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
@@ -13,6 +21,8 @@ class Student(models.Model):
     avatar = models.ImageField(null=True, blank=True, upload_to='media/')
     register_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+    year_in_school = models.CharField(
+        max_length=3, choices=YEAR_IN_SCHOOL_CHOICES, default="FR")
 
     def __str__(self):
         return f"{self.first_name}-{self.last_name}"
