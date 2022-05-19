39a50d3cc6d08d70f0b01745cb994ff729af0f3a
m_emin <mehmeteminbul@gmail.com>
2022-05-19 22:00:06 +0300

    session 2 ended

--- Forms/student/views.py
+++ Forms/student/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
-from django.shortcuts import render
+import profile
+from django.shortcuts import redirect, render
 
 from .forms import StudentForm
 
@@ -9,7 +10,16 @@ def index(request):
 
 
 def student_page(request):
-    form = StudentForm()
+    # print(request.POST)
+    form = StudentForm(request.POST or None)
+
+    if form.is_valid():
+        student = form.save()
+        if 'profile_pic' in request.FILES:
+            student.profile_pic = request.FILES['profile_pic']
+            student.save()
+        return redirect('index')
+        # print(form.cleaned_data.get('first_name'))
     context = {
         'form': form
     }
