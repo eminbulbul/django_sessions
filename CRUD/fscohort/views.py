5fde352ad44aa8df8eeb8febcc1192cc379fef3f
m_emin <mehmeteminbul@gmail.com>
2022-05-21 12:00:26 +0300

    session 1 ended

--- CRUD/fscohort/views.py
+++ CRUD/fscohort/views.py
@@ -1,4 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Student
+
 
 def index(request):
     return render(request, 'fscohort/index.html')
+
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        "student": students
+    }
+    return render(request, "fscohort/student_list.html", context)
