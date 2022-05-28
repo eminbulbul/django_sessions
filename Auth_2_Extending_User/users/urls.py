4ef27cc8b00b1988634b207a8bfc9f1ed20b3d23
m_emin <mehmeteminbul@gmail.com>
2022-05-28 12:34:12 +0300

    logout and register builded

--- Auth_2_Extending_User/users/urls.py
+++ Auth_2_Extending_User/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import user_logout
+from .views import user_logout, register
 urlpatterns = [
-    path('logout/', user_logout, name='logout')
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
 ]
--- Auth_2_Extending_User/users/views.py
+++ Auth_2_Extending_User/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 
 from django.contrib.auth import logout, login, authenticate
+from .forms import UserForm, UserProfileForm
 # Create your views here.
 
 
@@ -14,3 +15,14 @@ def user_logout(request):
     messages.success(request, 'You logged out!')
     logout(request)
     return redirect('home')
+
+
+def register(request):
+    form_user = UserForm()
+    form_profile = UserProfileForm()
+
+    context = {
+        "form_user": form_user,
+        "form_profile": form_profile,
+    }
+    return render(request, 'users/register.html', context)
