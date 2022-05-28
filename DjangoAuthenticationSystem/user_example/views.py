7900ec191a9e60661203009774ad7cf701a77a73
m_emin <mehmeteminbul@gmail.com>
2022-05-28 10:53:56 +0300

    session 1 begin

--- DjangoAuthenticationSystem/user_example/views.py
+++ DjangoAuthenticationSystem/user_example/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-def home_view(request):    
+
+def home_view(request):
     return render(request, "user_example/home.html")
 
 
-def special(request):    
+def special(request):
     return render(request, "user_example/special.html")
+
+
+def register(request):
+    form = UserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, "registration/register.html", context)
